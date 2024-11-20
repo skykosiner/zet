0b20a200d4e5e497a14140e84c0dc0ebdf4ac8b8
Sky Kosiner <sky@skykosiner.com>
2024-11-20 10:05:43 +0000

    wip(tags): Tags work excpet for yaml format

--- pkg/tags/tags.go
+++ pkg/tags/tags.go
@@ -8,14 +8,42 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"sort"
 	"strings"
 
 	"github.com/skykosiner/zet/pkg/config"
 	"github.com/skykosiner/zet/pkg/utils"
 )
 
-func exractTags(path string, tagSet map[string]struct{}, tagRegex *regexp.Regexp) {
+type tag struct {
+	tag  string
+	path string
+}
+
+type tags []tag
+
+func (t tag) String() string {
+	return fmt.Sprintf("%s: %s\n", t.path, t.tag)
+}
+
+func (t tags) getPaths() []string {
+	var paths []string
+	for _, tag := range t {
+		paths = append(paths, tag.path)
+	}
+
+	return paths
+}
+
+func (t tags) getTags() []string {
+	var tags []string
+	for _, tag := range t {
+		tags = append(tags, tag.tag)
+	}
+
+	return tags
+}
+
+func exractTags(path string, tagSet map[string]tag, tagRegex *regexp.Regexp) {
 	file, err := os.Open(path)
 	if err != nil {
 		slog.Error("Error opening file.", "error", err, "file path", path)
@@ -30,7 +58,10 @@ func exractTags(path string, tagSet map[string]struct{}, tagRegex *regexp.Regexp
 		matches := tagRegex.FindAllStringSubmatch(line, -1)
 		for _, match := range matches {
 			if len(match) > 1 {
-				tagSet[match[1]] = struct{}{}
+				tagSet[match[1]] = tag{
+					tag:  match[1],
+					path: path,
+				}
 			}
 		}
 	}
@@ -41,10 +72,9 @@ func exractTags(path string, tagSet map[string]struct{}, tagRegex *regexp.Regexp
 	}
 }
 
-func getTags(c config.Config, tagRegex *regexp.Regexp) []string {
-	var tags []string
-	tagSet := make(map[string]struct{})
-	// TODO: Get my regex license!
+func getTags(c config.Config, tagRegex *regexp.Regexp) tags {
+	var tags tags
+	tagSet := make(map[string]tag)
 
 	err := filepath.Walk(c.Vault, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
@@ -64,24 +94,23 @@ func getTags(c config.Config, tagRegex *regexp.Regexp) []string {
 	}
 
 	for tag := range tagSet {
-		tags = append(tags, tag)
+		tags = append(tags, tagSet[tag])
 	}
 
-	sort.Strings(tags)
 	return tags
 }
 
 // TODO: add in yaml stuff
 func Tags(c config.Config, fzfOptions string) {
+	// TODO: Get my regex license!
 	tags := getTags(c, regexp.MustCompile(`#(\w\S*)`))
-
 	if len(tags) == 0 {
 		slog.Info("Can't fand any tags.")
 		return
 	}
 
-	tag := utils.SearchFZF(fzfOptions, fmt.Sprintf("echo -e '%s'", strings.Join(tags, "\n")))
-	fmt.Println(tag)
+	tag := utils.SearchFZF(fzfOptions, fmt.Sprintf("echo -e '%s'", strings.Join(tags.getTags(), "\n")))
+	SearchByTag(c, fzfOptions, tag)
 }
 
 func SearchByTag(c config.Config, fzfOptions, tag string) {
@@ -90,4 +119,7 @@ func SearchByTag(c config.Config, fzfOptions, tag string) {
 		return
 	}
 
+	// TODO: Get my regex license!
+	tags := getTags(c, regexp.MustCompile(fmt.Sprintf(`#(%s\S*)`, tag)))
+	utils.OpenInEditor(utils.SearchFZF(fzfOptions, fmt.Sprintf("echo -e '%s'", strings.Join(tags.getPaths(), "\n"))))
 }
